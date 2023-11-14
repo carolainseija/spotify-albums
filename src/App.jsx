578a6816ac2e58074d6478c29c357442909371d4
carolainseija <carolainsilva1@gmail.com>
2023-11-14 12:46:29 -0300

    add less and create initial file with ant design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import { Flex } from "antd";
+import './theme.less';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <h1>Spotify Albums</h1>
-    </>
-  )
-}
+    <Flex gap="middle" align="start" vertical>
+      <Flex justify="center" align="center">
+        <h3  className="title-typography">Spotify Albums</h3>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default App
+export default App;
